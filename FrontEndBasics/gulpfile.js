28d7a44b9376c620e93606bbf9b1646ba306367f
Brett Whittington <brett.whittington@gmail.com>
2017-02-07 09:56:07 -0500

    Commit initial checking.

---
+++ FrontEndBasics/gulpfile.js
@@ -0,0 +1,37 @@
+﻿var gulp = require('gulp'),
+    concat = require("gulp-concat"),
+    sass = require("gulp-sass"),
+    rename = require("gulp-rename"),
+    uglify = require("gulp-uglify");
+
+gulp.task('sass', function () {
+    gulp.src('styles/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(rename('style.css'))
+      .pipe(gulp.dest('./styles/'))
+});
+
+gulp.task('vendorscripts', function () {
+    gulp.src(['bower_components/jquery/dist/jquery.js','bower_components/bootstrap-sass/assets/javascripts/bootstrap.js'])
+      .pipe(rename({
+          suffix: ".min",
+          extname: ".js"
+      }))
+      .pipe(concat.scripts('vendor-scripts.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('./scripts/'));
+});
+
+gulp.task('scripts', function () {
+    gulp.src('scripts/app/**/*.js')
+      .pipe(concat('scripts.js'))
+      //.pipe(uglify())
+      .pipe(gulp.dest('./scripts/'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('styles/scss/**/*.scss', ['sass']);
+    gulp.watch('scripts/app/**/*.js', ['scripts']);
+});
+
+gulp.task('default', ['vendorscripts', 'scripts', 'sass' ,'watch']);
---
+++ FrontEndBasics/scripts/app/browser-scripts.js
@@ -0,0 +1,26 @@
+﻿var requests = (function () {
+
+    var getResponse = function (cntrl) {
+        var myHeaders = new Headers();
+
+        myHeaders.append("Content-Type", "application/json");
+
+        var myInit = {
+            method: 'GET',
+            headers: myHeaders,
+            cache: 'default'
+        };
+
+        var myRequest = new Request('http://ipinfo.io/json', myInit);
+        fetch(myRequest)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            $('#' + cntrl).text(JSON.stringify(data));
+        });
+    }
+    return {
+        getResponse: getResponse
+    };
+})();
